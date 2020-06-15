506010126120d90f5461ede9707e701d19c4052f
herdiantyrahayu <63810292+herdiantyrahayu@users.noreply.github.com>
2020-06-15 17:31:57 +0800

    Add files via upload

---
+++ 1/doge.bat
@@ -0,0 +1,27 @@
+:back
+python bot.py 6283137494514 doge
+cls
+python bot.py 6283137494512 doge
+cls
+python bot.py 6283137494495 doge
+cls
+python bot.py 6283137494547 doge
+cls
+python bot.py 6283137494493 doge
+cls
+python bot.py 6283137494506 doge
+cls
+python bot.py 6283137494507 doge
+cls
+python bot.py 6283137494505 doge
+cls
+python bot.py 6283137494504 doge
+cls
+python bot.py 6283137494502 doge
+cls
+python bot.py 6283137494498 doge
+cls
+python bot.py 6283137494540 doge
+cls
+timeout 3600
+goto :back
---
+++ 1/zec.bat
@@ -0,0 +1,27 @@
+:back
+python bot.py 6283137494514 zec
+cls
+python bot.py 6283137494512 zec
+cls
+python bot.py 6283137494495 zec
+cls
+python bot.py 6283137494547 zec
+cls
+python bot.py 6283137494493 zec
+cls
+python bot.py 6283137494506 zec
+cls
+python bot.py 6283137494507 zec
+cls
+python bot.py 6283137494505 zec
+cls
+python bot.py 6283137494504 zec
+cls
+python bot.py 6283137494502 zec
+cls
+python bot.py 6283137494498 zec
+cls
+python bot.py 6283137494540 zec
+cls
+timeout 3600
+goto :back
